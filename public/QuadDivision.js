c984df676ef95f15cfb8efed665ee540be9a54e9
Daniel Marin <danmarcab@gmail.com>
2019-11-26 18:13:50 +0000

    Adjust public JS API

--- public/QuadDivision.js
+++ public/QuadDivision.js
@@ -1,27 +1,27 @@
 import {Elm} from '../src/Main.elm'
 
-window.startQuadDivision = ({node, elmUIEmbedded = false}) => {
-    const app = Elm.Main.init({
-        node,
-        flags: {
-            elmUIEmbedded, viewport:
-                {width: window.innerWidth, height: window.innerHeight}
-        }
-    });
+window.QuadDivision = {
+    init: ({node, flags}) => {
+        const finalFlags = Object.assign(flags, {
+            viewport: {width: window.innerWidth, height: window.innerHeight}
+        });
 
-    window.addEventListener("resize", function () {
-        app.ports.windowResizes.send({width: window.innerWidth, height: window.innerHeight});
-    });
+        const app = Elm.Main.init({node, flags: finalFlags});
 
-    app.ports.downloadSvg.subscribe(svgId => {
-        const svg = document.getElementById(svgId);
-        const svgAsXML = (new XMLSerializer).serializeToString(svg);
-        const dataURL = "data:image/svg+xml," + encodeURIComponent(svgAsXML);
+        window.addEventListener("resize", function () {
+            app.ports.windowResizes.send({width: window.innerWidth, height: window.innerHeight});
+        });
 
-        const dl = document.createElement("a");
-        document.body.appendChild(dl); // This line makes it work in Firefox.
-        dl.setAttribute("href", dataURL);
-        dl.setAttribute("download", `${svgId}.svg`);
-        dl.click();
-    });
+        app.ports.downloadSvg.subscribe(svgId => {
+            const svg = document.getElementById(svgId);
+            const svgAsXML = (new XMLSerializer).serializeToString(svg);
+            const dataURL = "data:image/svg+xml," + encodeURIComponent(svgAsXML);
+
+            const dl = document.createElement("a");
+            document.body.appendChild(dl); // This line makes it work in Firefox.
+            dl.setAttribute("href", dataURL);
+            dl.setAttribute("download", `${svgId}.svg`);
+            dl.click();
+        });
+    }
 };
